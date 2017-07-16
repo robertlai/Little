9709468ddbb6e88f4287f726a56a794c15c8fe76
Robert Lai <rcylai@hotmail.com>
2017-07-16 00:10:14 -0700

    Make publicCommands safe

--- app/commands/publicCommands.js
+++ app/commands/publicCommands.js
@@ -2,14 +2,19 @@ import data from '../data'
 import logger from '../logger'
 
 export default function publicCommands(message) {
-  const commands = data.matchCommands(message)
-  if (commands.length) {
-    logger.log('Executing public commands...')
-    logger.block([
-      `Commands: ${commands.map(command => command.name)}`
-    ])
-    commands.forEach(command => {
-      message.channel.send(command.output)
-    })
+  try {
+    const commands = data.matchCommands(message)
+    if (commands.length) {
+      logger.log('Executing public commands...')
+      logger.block([
+        `Commands: ${commands.map(command => command.name)}`
+      ])
+      commands.forEach(command => {
+        message.channel.send(command.output)
+      })
+    }
+  } catch (err) {
+    logger.log('Public commands matching failed.')
+    logger.error(err)
   }
 }
